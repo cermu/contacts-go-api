eaef9fa2aa34d12dec2949e35999dcb9aa7fb7ef
cermu <kwendosamson750@gmail.com>
2020-05-08 04:52:18 +0300

    moved file logging to utils package

--- models/database.go
+++ models/database.go
@@ -6,25 +6,27 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
-	"os"
+	utl "my-contacts/utils"
+	. "os"
 )
 
 var db *gorm.DB // database
 
 func init() {
 	// fmt.Println("Initializing database...")
-	log.Println("Initializing database...")
+	// log.Println("Initializing database...")
+	utl.WriteToFile("INFO | Initializing database...")
 	e := godotenv.Load() // Load .env file
 	if e != nil {
 		// fmt.Print(e)
 		log.Fatalf("The following error occurred while loading env file: %s", e)
 	}
 
-	username := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
-	dbType := os.Getenv("db_type")
+	username := Getenv("db_user")
+	password := Getenv("db_pass")
+	dbName := Getenv("db_name")
+	dbHost := Getenv("db_host")
+	dbType := Getenv("db_type")
 
 	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s",
 		dbHost, username, dbName, password) // Build connection string
@@ -32,10 +34,11 @@ func init() {
 
 	conn, err := gorm.Open(dbType, dbUri)
 	if err != nil {
-		// fmt.Print(err)
-		// fmt.Printf("Initializing database \t [FAIL]")
-		log.Printf("Initializing database \t [FAIL]")
-		log.Printf("The following error occurred while oppeinig DB connection: %s", err)
+		// log.Printf("Initializing database \t [FAIL]")
+		// log.Printf("The following error occurred while opening DB connection: %s", err)
+		utl.WriteToFile(fmt.Sprintf("ERROR | Initializing database \t [FAIL]"))
+		utl.WriteToFile(fmt.Sprintf("ERROR | The following error occurred while opening DB connection: %s",
+			err))
 	}
 
 	db = conn
@@ -43,8 +46,8 @@ func init() {
 	// db.Debug().AutoMigrate(&Account{}) // Database migration
 
 	if err == nil {
-		// fmt.Printf("Initializing database \t [OK]")
-		log.Printf("Initializing database \t [OK]")
+		// log.Printf("Initializing database \t [OK]")
+		utl.WriteToFile(fmt.Sprintf("INFO | Initializing database \t [OK]"))
 	}
 }
 
--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,14 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 // Message function builds json messages
@@ -14,4 +21,46 @@ func Message(status bool, message string) map[string]interface{} {
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(data)
+}
+
+// WriteToFile function create the logs dir and a monthly sub dir in it.
+// Then a log file is created to store the application logs.
+func WriteToFile(msg string) {
+	e := godotenv.Load() // Load .env file
+	if e != nil {
+		// fmt.Print(e)
+		log.Fatalf("The following error occurred while loading env file: %s", e)
+	}
+
+	DEBUG, boolErr := strconv.ParseBool(os.Getenv("DEBUG"))
+	if boolErr != nil {
+		log.Fatalf("Error parseing bool: %v", boolErr)
+	}
+
+	if DEBUG {
+		fmt.Println(msg)
+	} else {
+		currentDate := time.Now()
+		y, m, d := currentDate.Date()
+		truncateMonth := m.String()[:3]
+		dirName := "./logs/" + truncateMonth
+		logFile := strconv.Itoa(d) + strconv.Itoa(int(m)) + strconv.Itoa(y)
+
+		// Creating the log directory
+		dirErr := os.MkdirAll(dirName, os.ModePerm)
+		if dirErr != nil || os.IsExist(dirErr) {
+			log.Fatalf("Error creating directory: %v", dirErr)
+		}
+
+		// Creating the log file
+		f, er := os.OpenFile(dirName+"/api_log"+logFile+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if er != nil {
+			log.Fatalf("Error opening file: %v", er)
+		}
+		wrt := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(wrt)
+		log.Println(msg)
+
+		defer f.Close()
+	}
 }
