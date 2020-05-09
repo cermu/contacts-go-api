c91126b1b415402acdb99b1186a59c755ac9a254
cermu <kwendosamson750@gmail.com>
2020-05-09 16:16:24 +0300

    added router.go to house NewRouter function

---
+++ routers/router.go
@@ -0,0 +1,22 @@
+package routers
+
+import (
+	"github.com/gorilla/mux"
+	"my-contacts/app"
+)
+
+// NewRouter1 function for registering endpoints and their handlers.
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	router.Use(app.JwtAuthentication) // Attach the JWT middleware
+	api := router.PathPrefix("/api/v1").Subrouter()
+
+	for _, route := range routes {
+		api.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+	return router
+}
--- routers/routes.go
+++ routers/routes.go
@@ -1,8 +1,6 @@
 package routers
 
 import (
-	"github.com/gorilla/mux"
-	"my-contacts/app"
 	"my-contacts/controllers"
 	"net/http"
 )
@@ -15,22 +13,6 @@ type Route struct {
 }
 
 type Routes []Route
-
-// NewRouter function for registering endpoints and their handlers.
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	router.Use(app.JwtAuthentication) // Attach the JWT middleware
-	api := router.PathPrefix("/api/v1").Subrouter()
-
-	for _, route := range routes {
-		api.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
-	return router
-}
 
 var routes = Routes{
 	Route{
